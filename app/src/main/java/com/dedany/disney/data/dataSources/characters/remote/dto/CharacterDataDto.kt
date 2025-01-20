6dd7598a9b0fec1ae44008785d21981577c4548e
dperez <danylerez@gmail.com>
2025-01-20 13:36:27 +0100

    Add dto

---
+++ app/src/main/java/com/dedany/disney/data/dataSources/characters/remote/dto/CharacterDataDto.kt
@@ -0,0 +1,5 @@
+package com.dedany.disney.data.dataSources.characters.remote.dto
+
+data class CharacterDataDto(
+    val data: List<CharacterDto>
+)
---
+++ app/src/main/java/com/dedany/disney/data/dataSources/characters/remote/dto/CharacterDto.kt
@@ -0,0 +1,14 @@
+package com.dedany.disney.data.dataSources.characters.remote.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class CharacterDto(
+    @SerializedName("_id")
+    val id: Int,
+    val films: List<String>,
+    val shortFilms: List<String>,
+    val tvShows: List<String>,
+    val videoGames: List<String>,
+    val sourceUrl: String,
+    val imageUrl: String
+)
